99992c78d37fb60d1761ec941f310046f797a77b
Cristian <criricyt@gmail.com>
2022-06-23 23:59:03 -0300

    tudo

---
+++ untitled/cutellipsoid.h
@@ -0,0 +1,20 @@
+#ifndef CUTELLIPSOID_H
+#define CUTELLIPSOID_H
+
+#include "figurageometrica.h"
+
+class CutEllipsoid : public FiguraGeometrica{
+    protected:
+
+        int xcenter, ycenter, zcenter, rx, ry, rz;
+
+    public:
+
+        CutEllipsoid(int _xcenter, int _ycenter, int _zcenter, int _rx, int _ry, int _rz);
+
+        ~CutEllipsoid();
+
+        void draw(Sculptor &t);
+};
+
+#endif // CUTELLIPSOID_H
---
+++ untitled/cutvoxel.h
@@ -0,0 +1,20 @@
+#ifndef CUTVOXEL_H
+#define CUTVOXEL_H
+
+#include "figurageometrica.h"
+
+class CutVoxel : public FiguraGeometrica{
+    protected:
+
+        int x, y, z;
+
+    public:
+
+        CutVoxel(int _x, int _y, int _z);
+
+        ~CutVoxel();
+
+        void draw(Sculptor &t);
+};
+
+#endif // CUTVOXEL_H
